729a01fff81b468b112d286b565ab1ddc7e9bebb
fyusuf-a <fyusuf-a@student.42.fr>
2023-10-06 11:00:38 +0200

    feat(navbar): adds a navbar

---
+++ src/routes/Navbar.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let backgroundColor = "black";
+  export let color = "white";
+
+  const style = `background-color: ${backgroundColor}; color: ${color};`;
+</script>
+
+<nav style="{ style }">
+  <a href="/">Home</a>
+  <a href="/about">About</a>
+  <a href="/contact">Contact</a>
+</nav>
+
+<style lang="scss">
+  a {
+    @include impact;
+  }
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+        css: {
+          preprocessorOptions: {
+            scss: {
+              additionalData: `@import "./src/styles/variables.scss";`
+            }
+          }
+        },
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
