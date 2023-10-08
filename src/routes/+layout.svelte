625210167208e1dfd801c0dfca952a75f71b5bb8
fyusuf-a <fyusuf-a@student.42.fr>
2023-10-08 15:12:07 +0200

    feat(navbar): styles the navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script>
   import '../styles/global.scss';
   import Navbar from './Navbar.svelte';
+
+  const theme = {
+    backgroundColor: '#000',
+    color: '#fff'
+  };
 </script>
 
-<Navbar />
+<Navbar {theme} />
 <slot />
--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -1,18 +1,35 @@
 <script>
-  export let backgroundColor = 'black';
-  export let color = 'white';
-
-  const style = `background-color: ${backgroundColor}; color: ${color};`;
+  export let theme;
+  const style = `background-color: ${theme.backgroundColor}; color: ${theme.color};`;
 </script>
 
 <nav {style}>
-  <a href="/">Home</a>
-  <a href="/about">About</a>
-  <a href="/contact">Contact</a>
+  <div class="link-container">
+    <a href="/">Our product</a>
+    <a href="/plum-elixir">Plum elixir</a>
+  </div>
+  <div class="link-container">
+    <a href="/contact">Contact</a>
+  </div>
 </nav>
 
 <style lang="scss">
   a {
     @include impact;
+    font-size: 12px;
+  }
+
+  nav {
+    padding: 40px 60px;
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+  }
+
+  .link-container {
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: row;
+    gap: 40px;
   }
 </style>
