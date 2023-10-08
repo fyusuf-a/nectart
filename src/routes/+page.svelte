def343a0d161ffa1a14aa7dfc744bf822820108d
fyusuf-a <fyusuf-a@student.42.fr>
2023-10-08 20:02:56 +0200

    refactor: moves the welcome screen in /product

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,11 @@
-<script lang="ts">
-  import MouseAcceleratedVideo from '$lib/mouseAcceleratedVideo.svelte';
-  import { easeCubicFactory } from '$lib/easing';
+<script>
+  import { page } from '$app/stores';
+  import { goto } from '$app/navigation';
+  import { onMount } from 'svelte';
 
-  let easingParameters: [number, number, number, number] = [0.16, 1, 0.3, 1];
-  let easingFunction = easeCubicFactory(...easingParameters);
+  onMount(() => {
+    if ($page.url.pathname === '/') {
+      goto('/product');
+    }
+  });
 </script>
-
-<div class="section1">
-  <MouseAcceleratedVideo
-    cssSelector="video.mouseAccelerated"
-    targetReturnTime={1}
-    {easingFunction}
-  />
-  <div class="circle">
-    <video autoplay muted loop class="mouseAccelerated">
-      <source src="videos/oleg-lehnitsky.mp4" type="video/mp4" />
-    </video>
-  </div>
-</div>
-
-<style>
-  .section1 {
-    background-color: var(--Black);
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .circle {
-    width: 50vw;
-    height: 50vw;
-    background-color: red;
-    border-radius: 50%;
-    overflow: hidden;
-  }
-
-  .circle video {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
-</style>
---
+++ src/routes/product/+page.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+  import MouseAcceleratedVideo from '$lib/mouseAcceleratedVideo.svelte';
+  import { easeCubicFactory } from '$lib/easing';
+
+  let easingParameters: [number, number, number, number] = [0.16, 1, 0.3, 1];
+  let easingFunction = easeCubicFactory(...easingParameters);
+</script>
+
+<div class="section1">
+  <MouseAcceleratedVideo
+    cssSelector="video.mouseAccelerated"
+    targetReturnTime={1}
+    {easingFunction}
+  />
+  <div class="circle">
+    <video autoplay muted loop class="mouseAccelerated">
+      <source src="videos/oleg-lehnitsky.mp4" type="video/mp4" />
+    </video>
+  </div>
+  <div class="rocks">
+    <img src="photos/rocks.png" alt="" />
+  </div>
+</div>
+
+<style lang="scss">
+  .section1 {
+    background-color: var(--Black);
+    width: 100vw;
+    height: calc(100vh - 92px);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+  }
+
+  .circle {
+    width: 50vw;
+    height: 50vw;
+    background-color: green;
+    border-radius: 50%;
+    overflow: hidden;
+    position: absolute;
+    z-index: 1;
+  }
+
+  .circle video {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  .rocks img {
+    background-color: transparent;
+    position: absolute;
+    width: 100%;
+    bottom: calc(50% - 27vw);
+    left: 0;
+    right: 0;
+    z-index: 2;
+  }
+
+  .rocks {
+    position: relative;
+    width: 100vw;
+    height: calc(100vh - 92px);
+  }
+</style>
