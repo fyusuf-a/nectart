2a5749ee03a360392c80d12f10c80a5c617319ed
Florian Y <florian.y@galadrim.fr>
2023-10-16 10:34:51 +0200

    feat(p1s2): adds a video circle

--- src/routes/product/Section2.svelte
+++ src/routes/product/Section2.svelte
@@ -1,19 +1,34 @@
 <div id="section2">
-  <div id="carousel" />
+  <div class="current">
+    <video id="pine-video" autoplay loop>
+    <source src="videos/victor-rogov.mp4" type="video/mp4" />
+    </video>
+  </div>
 </div>
 
-<style>
+<style lang="scss">
   #section2 {
-    background-color: purple;
+    background-color: var(--White);
+    z-index: 3;
     height: 100vh;
     position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 
-  #carousel {
-    position: absolute;
-    z-index: 3;
+  .current {
+    position: relative;
+    width: circle-size(0.9);
+    height: circle-size(0.9);
+    border-radius: 50%;
+    overflow: hidden;
+    background-color: white;
+  }
+
+  .current video {
     width: 100%;
     height: 100%;
-    background-color: red;
+    object-fit: cover;
   }
 </style>
