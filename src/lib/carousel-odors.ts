56bd2cc59c4bd5673c7aa6d8c9bffbe0eca420b5
Florian Y <florian.y@galadrim.fr>
2023-10-18 12:54:53 +0200

    feat(assets): adds assets for the carousel

---
+++ src/lib/carousel-odors.ts
@@ -0,0 +1,27 @@
+export const odors = [
+  {
+    name: 'Bread crust',
+    photoUrl: 'conor-brown-WBYksGoFPb8.jpg',
+    videoUrl: 'bread-carlos-buitrago.mp4'
+  },
+  {
+    name: 'Vanilla',
+    photoUrl: 'sidath-vimukthi-fIGw-jVGy5g.jpg',
+    videoUrl: 'vanilla-rodnae-productions.mp4'
+  },
+  {
+    name: 'Saltwater',
+    photoUrl: 'marten-bjork-iXed2ZaTobo.jpg',
+    videoUrl: 'saltwater.mp4'
+  },
+  {
+    name: 'Pine',
+    photoUrl: 'darius-cotoi-5zLwelu7L8U.jpeg',
+    videoUrl: 'victor-rogov.mp4'
+  },
+  {
+    name: 'Petrichor',
+    photoUrl: 'jana-muller-2rCRXB4W6-0.jpg',
+    videoUrl: 'petrichor.mp4'
+  },
+];
