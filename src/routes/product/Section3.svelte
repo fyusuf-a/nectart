5e12dd89557649fca3029099dc2a446cec106279
Florian Y <florian.y@galadrim.fr>
2023-10-28 16:55:58 +0200

    feat(p1s2): styles the section

--- src/routes/product/Section3.svelte
+++ src/routes/product/Section3.svelte
@@ -1,9 +1,35 @@
-<div id="section3" />
+<div id="section3">
+  <div class="centered">Perfume <span class="accent">is</span><br> a sensation</div>
+</div>
 
-<style>
+<style lang="scss">
   #section3 {
-    background-color: blue;
+    opacity: 0.7;
+    /* background: url("./photos/mio-ito-B_SLtmXPKNA.jpg"),
+    * linear-gradient(0deg, #F8E2E0 0%, rgba(247, 223, 221, 0.00) 95.43%),
+    * #F9E5E3; */
+    background: url("./photos/mio-ito-B_SLtmXPKNA.jpg"), linear-gradient(0deg, #F8E2E0 0%, rgba(247, 223, 221, 0.00) 95.43%), #ffe0dd;
+    background-blend-mode: overlay, multiply;
     height: 100vh;
     z-index: 100;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
+
+  #section3 div {
+    @include impact;
+    @include typographic-scale(3, 0);
+
+    & .accent {
+      @include accent;
+    }
+
+  }
+
+  centered {
+    text-align: center;
+  }
+    
 </style>
+
