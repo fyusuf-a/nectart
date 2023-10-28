f02fbad0c60d36bf9b813bffcaedced879e1965a
Florian Y <florian.y@galadrim.fr>
2023-10-28 17:23:40 +0200

    feat(p1s3): creates a pin of the section

--- src/routes/product/Section3.svelte
+++ src/routes/product/Section3.svelte
@@ -1,3 +1,18 @@
+<script>
+  import { onMount } from 'svelte';
+  import ScrollTrigger from 'gsap/ScrollTrigger';
+
+  // Pin the section
+  onMount(() => {
+    ScrollTrigger.create({
+      trigger: '#section3',
+      pin: true,
+      end: 'bottom -100%'
+    });
+
+  });
+</script>
+
 <div id="section3">
   <div class="centered">Perfume <span class="accent">is</span><br> a sensation</div>
 </div>
@@ -11,7 +26,6 @@
     background: url("./photos/mio-ito-B_SLtmXPKNA.jpg"), linear-gradient(0deg, #F8E2E0 0%, rgba(247, 223, 221, 0.00) 95.43%), #ffe0dd;
     background-blend-mode: overlay, multiply;
     height: 100vh;
-    z-index: 100;
     display: flex;
     justify-content: center;
     align-items: center;
