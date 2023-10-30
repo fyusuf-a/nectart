98fddc8a272bc5cddbe9460f7e1cfb38686aaf89
Florian Y <florian.y@galadrim.fr>
2023-10-30 16:46:20 +0100

    feat(p1s3): styles the second subsection

--- src/routes/product/section3/Subsection2.svelte
+++ src/routes/product/section3/Subsection2.svelte
@@ -23,18 +23,45 @@
 </script>
 
 <div class="subsection2">
+  <div class="background"/>
   <div id="dialogue"> A dialogue between</div>
   <div class="banner">
     the soul
   </div>
-  <div>
+  <div class="banner">
     the senses
   </div>
   <Banner />
 </div>
 
-<style>
+<style lang="scss">
 .subsection2 {
   height: 100vh;
+  position: relative;
+  z-index: -2;
+}
+
+.background {
+  background-color: #bdbdbd;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -4;
+}
+
+#dialogue {
+  @include accent;
+  @include typographic-scale(1, 1);
+  color: var(--Black);
+  text-align: center;
+  z-index: 4;
+}
+
+.banner {
+  @include impact;
+  @include typographic-scale(3, 0);
+  color: var(--White);
 }
 </style>
