4ed0af27944595156f8b1ea32e4c0619601532e2
Florian Y <florian.y@galadrim.fr>
2023-11-13 10:20:47 +0100

    feat(perfume is art): adds the section

--- src/routes/product/+page.svelte
+++ src/routes/product/+page.svelte
@@ -2,6 +2,7 @@
   import Section1 from './Section1.svelte';
   import Section2 from './Section2.svelte';
   import Section3 from './Section3.svelte';
+  import Section4 from './PerfumeIsArt.svelte';
   import Bottle from './Bottle.svelte';
   
   const bottleBlur = '10px';
@@ -11,7 +12,8 @@
 <Section1 />
 <Section2 />
 <Section3 />
-<div id="section4" />
+<Section4 />
+<div id="section5" />
 
 <style lang="scss">
   @import "./style.scss";
@@ -19,8 +21,9 @@
   :global(body) {
     background-color: var(--Black);
   }
-  #section4 {
-    background-color: purple;
+
+  #section5 {
     height: 100vh;
+    background-color: red;
   }
 </style>
---
+++ src/routes/product/PerfumeIsArt.svelte
@@ -0,0 +1,197 @@
+<script lang="ts">
+  import HollowedArt from './perfume-is-art/HollowedArt.svelte';
+  import gsap from 'gsap';
+  import ScrollTrigger from 'gsap/ScrollTrigger';
+  import { onMount } from 'svelte';
+  import SplitType from 'split-type';
+  import { lenis } from '../../stores/lenis';
+
+  let section: HTMLElement;
+  let sentenceStart: HTMLElement;
+  let firstChild: HTMLDivElement;
+  let secondChild: HTMLDivElement;
+  let video: HTMLVideoElement;
+
+  onMount(() => {
+    sentenceStart.style.opacity='0';
+    firstChild.style.display='none';
+    secondChild.style.display='none';
+    video.style.bottom = '50vh';
+    video.style.scale = '0.69';
+    video.style.opacity = '0';
+
+    new SplitType('#perfume-is-art .sentence-start', {
+      types: ['words', 'chars'],
+    });
+
+    let secondChildAlreadyDisplayed = false;
+
+    gsap.timeline({
+      scrollTrigger: {
+        trigger: '#perfume-is-art',
+        start: 'top top',
+        end: 'bottom top',
+        pin: true,
+      },
+    })
+      .to(
+        'html',
+        {
+          duration: 3,
+        }
+      );
+      
+
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: '#perfume-is-art',
+        start: 'top top',
+        end: 'bottom top',
+      },
+      onStart() {
+        sentenceStart.style.opacity='1';
+      },
+    });
+
+    const words = section.querySelectorAll('.sentence-start .word');
+    words.forEach((_, i) => {
+      const delay = i;
+      timeline.from(`#perfume-is-art .sentence-start .word:nth-child(${i + 1}) .char`, {
+        opacity: 0,
+        y: -20,
+        duration: 1,
+        delay,
+        stagger: 0.04,
+      }, '<');
+    })
+    timeline.to(
+      video.style,
+      {
+        bottom: '20vh',
+        duration: 1.25,
+        ease: 'power4.out',
+        scale: 1,
+        onStart: () => {
+          firstChild.style.display='block';
+          video.style.opacity = '1';
+        },
+        onUpdate: function() {
+          const progress = this.progress();
+          if (!secondChildAlreadyDisplayed && progress > 0.11) {
+            secondChild.style.display='block';
+            secondChildAlreadyDisplayed = true;
+
+          }
+        },
+      }
+    );
+  });
+</script>
+
+<section id="perfume-is-art" bind:this={section}>
+<div class="top-container">
+  <div class="sentence-start" bind:this={sentenceStart}>Perfume is</div>
+</div>
+
+<div class='hollowed-art-container'>
+  <div class='hollowed-art' bind:this={firstChild}>
+    <HollowedArt />
+  </div>
+  <div class='hollowed-art' bind:this={secondChild}>
+    <HollowedArt />
+  </div>
+</div>
+
+<video
+  autoplay
+  muted
+  loop
+  class="svg-clipped-text"
+  bind:this={video}
+>
+  <source
+    src="videos/oleg-lehnitsky.mp4"
+    type="video/mp4"
+  />
+</video>
+
+<svg
+  version="1.1"
+  width="0"
+  height="0"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <defs>
+  <clipPath id="svgTextPath">
+    <text
+      x="50vw"
+      text-anchor="middle"
+    >
+      Art
+    </text>
+  </clipPath>
+  </defs>
+</svg>
+
+</section>
+
+<style lang="scss">
+$text-height: $impact-line-height * scale(4,1);
+
+#perfume-is-art {
+  position: relative;
+  height: 100vh;
+}
+
+.top-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  height: 20vh;
+}
+
+.sentence-start {
+  @include accent;
+  @include typographic-scale(2, 0);
+  color: var(--White);
+}
+
+.svg-clipped-text {
+  position: absolute;
+  bottom: 20vh;
+  -webkit-clip-path: url(#svgTextPath);
+  clip-path: url(#svgTextPath);
+}
+
+video {
+  width: 100%;
+  height: $text-height;
+  object-fit: cover;
+  object-position: center;
+}
+
+svg text {
+  @include impact;
+  @include typographic-scale(4, 1);
+  transform: translate(0, $text-height);
+  stroke-width: 1px;
+  stroke: url(#paint0_linear_377_166);
+}
+
+.hollowed-art:nth-child(n) {
+  position: absolute;
+  width: 100%;
+  left: 0;
+}
+
+.hollowed-art:nth-child(1) {
+  bottom: 50vh;
+  scale: 0.69;
+}
+
+.hollowed-art:nth-child(2) {
+  bottom: 35.7vh;
+  scale: 0.86;
+}
+</style>
