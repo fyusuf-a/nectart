877e40848fccfe3124ab97154b50d2c21e21664a
Florian Y <florian.y@galadrim.fr>
2023-11-18 18:35:09 +0100

    feat(whisper): adds slide content

--- src/routes/plum-elixir/whisper/Subsection2.svelte
+++ src/routes/plum-elixir/whisper/Subsection2.svelte
@@ -7,73 +7,48 @@
   const slides = [
     {
       src: 'anuj-yadav-W1jNzyZFmIU-unsplash.jpg',
-      order: 0,
-      text: 'A sultry dance&emdash;',
-    },
-    {
-      src: 'wine.png',
-      order: 7,
-      text: 'a liquid sonnet',
+      position: 0,
+      text: 'A sultry dance&mdash;',
     },
     {
       src: 'loes-klinker-T5VHI-Pj2NQ-unsplash.jpg',
-      order: 1,
-      text: 'of aromas',
+      text: 'A sultry dance&mdash;of aromas',
+      position: 2,
     },
     {
       src: 'pexels-studio-naae-17795179.jpg',
-      order: 2,
-      text: 'A breath of mediteranean breeze',
-    },
-    {
-      src: 'vlad-kutepov-OBR9-lQE_H4-unsplash.jpg',
-      order: 4,
-      text: 'aged in antique cellars',
+      text: 'A sweet whisper',
+      position: 3,
     },
     {
       src: 'anthony-salerno-I1hzGTtKMgU-unsplash.jpg',
-      order: 3,
-      text: 'a sweet whisper',
+      position: 5,
+      text: 'A breath of mediterranean breeze',
+    },
+    {
+      src: 'vlad-kutepov-OBR9-lQE_H4-unsplash.jpg',
+      position: 4,
+      text: ' aged in antique cellars',
     },
     {
       src: 'glasses.png',
-      order: 5,
-      text: 'a ripe jewel',
+      position: 6,
+      text: 'A ripe jewel',
     },
     {
       src: 'monika-grabkowska--1PzCC5XAzo-unsplash.jpg',
-      order: 6,
-      text: 'bursting with sugar'
-    }
+      position: 7,
+      text: 'A ripe jewel bursting with sugar',
+    },
+    {
+      src: 'wine.png',
+      position: 1,
+      text: 'A liquid sonnet',
+    },
   ]
+  let currentSlideIndex = 0;
+
   onMount(() => {
-    const bringToView = (imgIndex: number, timeline: gsap.core.Timeline) => {
-      return timeline.to(`#whisper .img-${imgIndex}`, {
-        duration: 1,
-        top: '0vh',
-        height: '100vh',
-        ease: 'power2.inOut'
-      }
-      , '<');
-    }
-
-    const baseHeight = 100 / 17;
-
-    const slideToNthPosition = (imgIndex: number, destinationIndex: number, timeline: gsap.core.Timeline) => {
-      timeline.fromTo(`#whisper .img-${imgIndex}`, {
-        height: '100%',
-      }, {
-        duration: 1,
-        height: `${3 * baseHeight}%`,
-        top: `calc(${1 + 4 * (imgIndex % 4)} * ${baseHeight}vh)`,
-      })
-        .to(`#whisper .img-${imgIndex}`, {
-          duration: 1,
-          width: '15vw',
-          left: destinationIndex < 4 ? '0' : '85vw',
-          ease: 'power2.inOut'
-        })
-    }
 
     // initializes the styles
     subsection.querySelectorAll('.img-container').forEach((img, i) => {
@@ -83,28 +58,61 @@
       img.style.left = '20vw';
     });
 
-    const timeline = gsap.timeline({
-      scrollTrigger: {
-        trigger: subsection,
-        start: 'top top',
-        end: '+800%',
-        scrub: true,
-        pin: true,
-        markers: true
+
+    const gsapContext = gsap.context(() => {
+      const bringToView = (imgIndex: number, timeline: gsap.core.Timeline) => {
+        return timeline.to(`#whisper .img-${imgIndex}`, {
+          duration: 1,
+          top: '0vh',
+          height: '100vh',
+          ease: 'power2.inOut',
+          onStart() {
+            currentSlideIndex = imgIndex;
+          },
+          onReverseComplete: () => {
+            currentSlideIndex = imgIndex - 1;
+          }
+        }
+        , '<');
+      }
+
+      const baseHeight = 100 / 17;
+
+      const slideToNthPosition = (imgIndex: number, destinationIndex: number, timeline: gsap.core.Timeline) => {
+        timeline.fromTo(`#whisper .img-${imgIndex}`, {
+          height: '100%',
+        }, {
+          duration: 1,
+          height: `${3 * baseHeight}%`,
+          top: `calc(${1 + 4 * (destinationIndex % 4)} * ${baseHeight}vh)`,
+        })
+          .to(`#whisper .img-${imgIndex}`, {
+            duration: 1,
+            width: '15vw',
+            left: destinationIndex < 4 ? '0' : '85vw',
+            ease: 'power2.inOut'
+          })
+      }
+
+      const timeline = gsap.timeline({
+        scrollTrigger: {
+          trigger: subsection,
+          start: 'top top',
+          end: '+800%',
+          scrub: true,
+          pin: true,
+          markers: true
+        }
+      });
+
+      for (let i = 0; i < 8; i++) {
+        if (i !== 0)
+          bringToView(i, timeline);
+        console.log('slides', i, slides[i]);
+        slideToNthPosition(i, slides[i].position, timeline);
       }
     });
 
-    for (let i = 0; i < 8; i++) {
-      const currentSlide = slides.findIndex(slide => slide.order === i);
-      if (currentSlide !== 0)
-        bringToView(currentSlide, timeline);
-      slideToNthPosition(currentSlide, currentSlide, timeline);
-    }
-    bringToView(8, timeline);
-
-    const gsapContext = gsap.context(() => {
-
-    });
     return () => {
       gsapContext.revert();
     };
@@ -120,6 +128,20 @@
         src={`photos/whisper/${slide.src}`}
       />
     </div>
+    <div class="text-container">
+      <div
+        class="text-stack layer"
+        class:active={i === currentSlideIndex}
+      >
+        {@html slide.text }
+      </div>
+      <div
+        class="text-stack"
+        class:active={i === currentSlideIndex}
+      >
+        {@html slide.text }
+      </div>
+  </div>
   {/each}
 </div>
 
@@ -136,6 +158,7 @@
   .subsection2 {
     position: relative;
     height: 100vh;
+    background: var(--Grey);
   }
 
   .img-container {
@@ -162,5 +185,38 @@
       top: calc(#{(1 + 4 * ($i - 4))} * $lateral-height);
       right: 0;
     }
+  }
+
+  .text-container {
+    position: absolute;
+    top: 0;
+    left: 20vw;
+    width: 60vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .text-stack {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    left: 0;
+    width: 60vw;
+    @include typographic-scale(2, 1);
+    text-align: center;
+    color: var(--White);
+    mix-blend-mode: exclusion;
+    visibility: hidden;
+  }
+
+  .text-stack.layer {
+    color: black;
+    mix-blend-mode: color;
+  }
+
+  .text-stack.active {
+    visibility: visible;
   }
 </style>
