810f655e7f43250e59b60f2b18cfdd180e30cffc
Florian Y <florian.y@galadrim.fr>
2023-12-24 19:31:54 +0100

    feat(video): makes videos responsive

--- src/lib/Video.svelte
+++ src/lib/Video.svelte
@@ -1,8 +1,54 @@
 <script lang="ts">
-  export let video: HTMLVideoElement;
+  import { onMount } from "svelte";
+  import Image from "./Image.svelte";
+
+  export let src = '';
+  export let video: HTMLVideoElement | null = null;
+  export let poster: HTMLImageElement | null = null;
+  const resolutions = [1800, 1400, 1000, 800, 400, 200];
+  let videoAvailable = false;
+
+  onMount(() => {
+    const screenSize = window.innerWidth;
+    let videoResolution;
+    for (let i = 0; i < resolutions.length; i++) {
+      videoResolution = resolutions[i];
+      if (resolutions[i] < screenSize) {
+        break;
+      }
+    }
+    video!.src = `${src}-${videoResolution}.webm`;
+    video!.addEventListener('canplaythrough', () => {
+      video!.play();
+      videoAvailable = true;
+    });
+  })
 </script>
 
 <!-- svelte-ignore a11y-media-has-caption -->
-<video preload="metadata" {...$$restProps} bind:this={video}>
-  <slot />
-</video>
+<div class="video-container">
+  <Image
+    src={src}
+    image={poster ?? undefined}
+    style={$$restProps.style + (videoAvailable ? 'display: none;' : '')}
+    alt=""
+  />
+  <video
+    preload="metadata"
+    class="video"
+    bind:this={video}
+    {...$$restProps}
+  />
+</div>
+
+<style lang="scss">
+  .video-container {
+    width: 100%;
+    height: 100%;
+  }
+
+  .video {
+    width: 100%;
+    height: 100%;
+  }
+</style>
