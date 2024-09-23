dd284ab879e88bfeb9ff9a0a4e5a3f3a47d29088
fyusuf-a <fyusuf-a@ashiba.dev>
2024-09-23 17:04:20 +0200

    fix(frontend): redirects correctly to /product

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,5 +3,7 @@ import { redirect } from '@sveltejs/kit';
 /** @type {import('@sveltejs/kit').LayoutServerLoad} */
 export function load({ url }: { url: URL }) {
   const path = url.pathname;
-  if (path.startsWith('/')) redirect(302, '/product');
+  if (path === '/') {
+    throw redirect(302, '/product');
+  }
 }
