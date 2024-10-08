81edb5c10927c720de5fe5e71bbe1f20e9b2120b
fyusuf-a <florian.yusuf-ali@ashiba.dev>
2024-10-09 01:48:07 +0200

    chore: adds a script to launch a local validator with metaplex programs

---
+++ scripts/solana-test-validator.sh
@@ -0,0 +1,4 @@
+solana-test-validator -r \
+  --bpf-program CoREENxT6tW1HoK8ypY1SxRMZTcVPm7R94rH4PZNhX7d ./bin/core.so \
+  --bpf-program CMACYFENjoBMHzapRXyo1JZkVS6EtaDDzkjMrmQLvr4J ./bin/candy_machine_core.so \
+  --bpf-program CMAGAKJ67e9hRZgfC5SFTbZH8MgEmtqazKXjmkaJjWTJ ./bin/candy_guard.so
