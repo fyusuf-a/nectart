d2b9fe61a32026ddefb29bef2e2d6f992e6fe707
fyusuf-a <florian.yusuf-ali@ashiba.dev>
2024-10-08 21:32:07 +0200

    feat: adds the creation of a candy machine

--- src/routes/api/projects/+server.ts
+++ src/routes/api/projects/+server.ts
@@ -5,6 +5,28 @@ import { ZodError } from 'zod';
 import { prisma } from '@/lib/api/prisma';
 import { del, put } from '@vercel/blob';
 import { Project } from '@prisma/client';
+import { addConfigLines, create as createCandyMachine } from '@metaplex-foundation/mpl-core-candy-machine';
+import { generateSigner, PublicKey, sol, some } from '@metaplex-foundation/umi';
+import { umi } from '@/lib/blockchain/backendUmi';
+import { sendAndConfirmTransaction } from '@/lib/blockchain/utils';
+import { createCollectionV1 } from '@metaplex-foundation/mpl-core';
+
+const loadCandyMachine = async (candyMachinePublicKey: PublicKey, tokenNumber: number, projectId: string) => {
+  for (let i = 0; i < tokenNumber;) {
+    const arrayLength = i < tokenNumber - 20 ? 20 : tokenNumber % 20;
+    const configLines = Array.from({ length: arrayLength }, (_, j) => ({
+      uri: `${projectId}.json`,
+      name: `${i + j}`,
+    }));
+    const configLinesTransaction = addConfigLines(umi, {
+      candyMachine: candyMachinePublicKey,
+      index: i,
+      configLines,
+    });
+    await sendAndConfirmTransaction(umi, configLinesTransaction);
+    i += arrayLength;
+  }
+}
 
 export const GET: RequestHandler = async() => {
   const projects = await prisma.project.findMany({
@@ -22,6 +44,7 @@ export const GET: RequestHandler = async() => {
 }
 
 export const POST: RequestHandler = async({ request, locals }) => {
+  await umi.rpc.airdrop(umi.identity.publicKey, sol(2));
   let result: FormSchemaType;
   try {
     const body = await request.formData();
@@ -51,6 +74,7 @@ export const POST: RequestHandler = async({ request, locals }) => {
           heartNotes: result.heartNotes,
           userId: locals.user.id,
           budgetInSol: BUDGET_IN_SOL,
+          tokenNumber: result.tokenNumber,
         },
       });
       const { url } = await put(`projects/${project.id}/1`, result.picture, { access: 'public' });
@@ -60,8 +84,50 @@ export const POST: RequestHandler = async({ request, locals }) => {
           projectId: project.id,
         },
       });
+
+      const candyMachine = generateSigner(umi);
+      const collectionMint = generateSigner(umi);
+      const treasury = generateSigner(umi);
+      await tx.candyMachine.create({
+        data: {
+          projectId: project.id,
+          key: candyMachine.secretKey.toString(),
+          collectionKey: collectionMint.secretKey.toString(),
+          treasuryKey: treasury.secretKey.toString(),
+        },
+      });
+      await createCollectionV1(umi, {
+        collection: collectionMint,
+        name: result.name,
+        uri: `${import.meta.env.VITE_URL}/metadata/${project.id}.json`,
+      }).sendAndConfirm(umi)
+      const transactionBuilder = await createCandyMachine(umi, {
+        candyMachine,
+        collection: collectionMint.publicKey,
+        collectionUpdateAuthority: umi.identity,
+        itemsAvailable: result.tokenNumber,
+        authority: umi.identity.publicKey,
+        isMutable: false,
+        configLineSettings: some({
+            prefixName: '',
+            nameLength: 11,
+            prefixUri: `${import.meta.env.VITE_URL}/metadata/`,
+            uriLength: 40,
+            isSequential: false,
+        }),
+        guards: {
+          botTax: some({ lamports: sol(0.001), lastInstruction: true }),
+          solPayment: some({ lamports: sol(project.budgetInSol / project.tokenNumber), destination: treasury.publicKey }),
+        },
+      });
+      await sendAndConfirmTransaction(umi, transactionBuilder);
+      await loadCandyMachine(candyMachine.publicKey, result.tokenNumber, project.id);
+    },
+    {
+      timeout: 120000,
     });
   } catch (e) {
+    console.error(e);
     if (project) {
       await del(`projects/${(project as Project).id}/1`);
     }
